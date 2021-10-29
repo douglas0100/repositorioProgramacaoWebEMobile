ebcf06405a2f67e57b0128d4535854dd67ec055d
douglas leite <douglasfelipe0100@gmail.com>
2021-10-29 03:31:26 -0300

    adiciona javascript

--- 13 - DesafioLiferay/fila.js
+++ 13 - DesafioLiferay/fila.js
@@ -1,15 +1,51 @@
+const btEqueue = document.getElementById("btEqueue");
+const btDequeue = document.getElementById("btDequeue");
+const inputName = document.getElementById("inputName");
+const inputColor = document.getElementById("colors");
+const client = document.getElementById("client");
+
 function Queue() {
-    this.elements = [];
+
+    list = [];
+
+    Queue.prototype.equeue = function () {
+        
+        let name = inputName.value.trim();
+        let color = inputColor.value.trim();
+        let newClient = clientFactory(name, color);
+        if (!name) {
+            alert("Digite um nome!");
+            return;
+        }
+        list.push(newClient);
+        inputName.value = "";
+        inputName.focus();
+        client.innerHTML = `${newClient.name}`;
+        console.log("equeue " + `${list.length}`);
+    }
+
+    Queue.prototype.dequeue = function () {
+        if(list.length > 0){
+            list.shift();
+            console.log("dequeu " + `${list.length}`);
+        }else{
+            alert("Não há objetos na fila.")
+        }
+    }
+
+
 }
 
-Queue.prototype.enqueue = function (e) {
-    this.elements.push(e);
+
+function clientFactory(name, color) {
+    let client = {};
+    client.name = name;
+    client.color = color;
+    return client;
 }
 
-Queue.prototype.dequeue = function () {
-    return this.elements.shift();
-}
 
-Queue.prototype.isEmpty = function () {
-    return this.elements.length == 0;
-}
+var Queue = new Queue();
+
+btEqueue.onclick = Queue.equeue;
+btDequeue.onclick = Queue.dequeue;
